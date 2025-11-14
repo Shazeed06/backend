342e6e891b9b80aca815594d47bc0bfa9309dc5a
Shazeed06 <mdshazeed1234@gmail.com>
2025-11-15 00:56:05 +0530

    github data added

--- index.js
+++ index.js
@@ -1,6 +1,44 @@
+require('dotenv').config()
+
 const express = require('express')
 const app = express()
 const port = 3000
+
+const githubData = {
+  "login": "Shazeed06",
+  "id": 116747534,
+  "node_id": "U_kgDOBvVtDg",
+  "avatar_url": "https://avatars.githubusercontent.com/u/116747534?v=4",
+  "gravatar_id": "",
+  "url": "https://api.github.com/users/Shazeed06",
+  "html_url": "https://github.com/Shazeed06",
+  "followers_url": "https://api.github.com/users/Shazeed06/followers",
+  "following_url": "https://api.github.com/users/Shazeed06/following{/other_user}",
+  "gists_url": "https://api.github.com/users/Shazeed06/gists{/gist_id}",
+  "starred_url": "https://api.github.com/users/Shazeed06/starred{/owner}{/repo}",
+  "subscriptions_url": "https://api.github.com/users/Shazeed06/subscriptions",
+  "organizations_url": "https://api.github.com/users/Shazeed06/orgs",
+  "repos_url": "https://api.github.com/users/Shazeed06/repos",
+  "events_url": "https://api.github.com/users/Shazeed06/events{/privacy}",
+  "received_events_url": "https://api.github.com/users/Shazeed06/received_events",
+  "type": "User",
+  "user_view_type": "public",
+  "site_admin": false,
+  "name": "Md Shazeed",
+  "company": null,
+  "blog": "",
+  "location": "delhi",
+  "email": null,
+  "hireable": null,
+  "bio": null,
+  "twitter_username": null,
+  "public_repos": 10,
+  "public_gists": 0,
+  "followers": 1,
+  "following": 4,
+  "created_at": "2022-10-26T16:17:47Z",
+  "updated_at": "2025-11-05T18:56:51Z"
+}
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -15,8 +53,18 @@ app.get('/login',(req,res)=>{
 })
 
 app.get('/youtube',(req,res)=>{
-    res.send("<h2>hello Mr. Md Shazeed</h2>")
+    res.send("<h2>hello Md Shazeed</h2>")
 })
+
+
+app.get('/github',(req,res)=>{
+  res.json(githubData)
+})
+
+/*app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+*/
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
